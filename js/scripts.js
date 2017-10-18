c2a6dadd9c04715cd2c2c1e3127c3e2c63204c00
Ginger Lee Kretschmer and Sam Kissee <sam@>
2017-10-18 14:25:32 -0700

    update survery with response forms and success message

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
-$(function() {
-  $("form").submit(fuction(event) {
+$(document).ready(function() {
+  $("#form").submit(function(event) {
     event.preventDefault();
     var inputName = $("input#name").val();
+    var inputFood = $("input#food").val();
+    var inputMusic = $("input#music").val();
+    var inputNumber = $("#number").val();
+    var inputBorn = $("#born").val();
+    var inputColor = $("#color").val();
+    var inputFlavor = $("input:radio[name=flavor]:checked").val();
+    var inputDescription = $("#description").val();
+
+    $(".name").text(inputName);
+    $(".food").text(inputFood);
+    $(".music").text(inputMusic);
+    $(".number").text(inputNumber);
+    $(".born").text(inputBorn);
+    $(".color").text(inputColor);
+    $(".flavor").text(inputFlavor);
+    $(".description").text(inputDescription);
+    $(".success").show();
   });
 });
